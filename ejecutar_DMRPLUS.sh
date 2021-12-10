05d9cbbec828b708e8159d598f6bf31566ec5e9b
Manel <ea3eiz@ea3eiz.com>
2021-12-10 17:23:18 +0100

    1

--- ejecutar_DMRPLUS.sh
+++ ejecutar_DMRPLUS.sh
@@ -45,7 +45,7 @@ sudo ./MMDVMPLUS MMDVMPLUS.ini
 #sudo ./MMDVMPLUS MMDVMPLUS.ini
 cd /home/pi/Desktop
 sudo cp Abrir_MMDVMPLUS.desktop /home/pi
-sudo sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=88x19 --title=DMR+ -e sudo sh ejecutar_dmrplus_new.sh'" /home/pi/Abrir_MMDVMPLUS.desktop
+sudo sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=88x19 --title=DMR+ -e sudo sh ejecutar_DMRPLUS.sh'" /home/pi/Abrir_MMDVMPLUS.desktop
 sudo sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_DMRPLUS_OFF.png" /home/pi/Abrir_MMDVMPLUS.desktop
 sudo sed -i "10c Name[es_ES]=Abrir DMR+" /home/pi/Abrir_MMDVMPLUS.desktop
 sudo sed -i "6c MMDVMPLUS=OFF" /home/pi/status.ini
