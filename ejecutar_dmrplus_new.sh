3c79c3e0af76a321b14ff4537b693617b000442b
Manel <ea3eiz@ea3eiz.com>
2021-12-10 17:16:28 +0100

    1

--- ejecutar_dmrplus_new.sh
+++ ejecutar_dmrplus_new.sh
@@ -5,7 +5,7 @@ caracteres=`expr index $mode $buscar`
 caracteres_linea=`expr $caracteres - 1`
 numero_linea_port=`expr substr $mode 1 $caracteres_linea`
 mode=$(awk "NR==$numero_linea_port" /home/pi/MMDVMHost/MMDVMPLUS.ini)
-puerto=`expr substr $mode 11 14`
+puerto=`expr substr $mode 15 14`
 puerto="  "$puerto
 cd /home/pi/Desktop
 sudo cp RXF_DMRPLUS.desktop /home/pi
