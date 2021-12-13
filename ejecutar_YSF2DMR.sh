e8029d3cd2fc1c5887588dc9585e6d7530814dda
Manel <ea3eiz@ea3eiz.com>
2021-12-13 20:28:13 +0100

    1

--- ejecutar_YSF2DMR.sh
+++ ejecutar_YSF2DMR.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-mode=`grep -n -m 1 "^Port=" /home/pi/MMDVMHost/MMDVMFUSION.ini`
+#Colores 
+ROJO="\033[1;31m"
+VERDE="\033[1;32m"
+BLANCO="\033[1;37m"
+AMARILLO="\033[1;33m"
+CIAN="\033[1;36m"
+GRIS="\033[0m"
+
+mode=`grep -n -m 1 "^UARTPort=" /home/pi/MMDVMHost/MMDVMFUSION.ini`
 buscar=":"
 caracteres=`expr index $mode $buscar`
 caracteres_linea=`expr $caracteres - 1`
 numero_linea_port=`expr substr $mode 1 $caracteres_linea`
 mode=$(awk "NR==$numero_linea_port" /home/pi/MMDVMHost/MMDVMFUSION.ini)
-puerto=`expr substr $mode 11 9`
+puerto=`expr substr $mode 15 14`
 puerto="  "$puerto
 cd /home/pi/Desktop
 sudo cp RXF_YSF2DMR.desktop /home/pi
 frecuencia=$(awk "NR==13" /home/pi/MMDVMHost/MMDVMFUSION.ini)
-frecuencia=`expr substr $frecuencia 13 9`
+frecuencia=`expr substr $frecuencia 13 17`
 frecuencia=$frecuencia$puerto
 sed -i "11c Name=$frecuencia" /home/pi/RXF_YSF2DMR.desktop
 
