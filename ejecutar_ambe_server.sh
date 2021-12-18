b8729a40a2365d868fe1eb0e68f7b9e84e144a29
Manel <ea3eiz@ea3eiz.com>
2021-12-18 17:17:50 +0100

    1

--- ejecutar_ambe_server.sh
+++ ejecutar_ambe_server.sh
@@ -25,7 +25,7 @@ sleep 1
 
 cd /home/pi/Desktop
 sudo cp Abrir_ambe_server.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=72x15 -e sudo sh ejecutar_ambe_server.sh'" /home/pi/Abrir_ambe_server.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/A108; ./qt_editor_ambe_3003'" /home/pi/Abrir_ambe_server.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_AMBE_SERVER_OFF.png" /home/pi/Abrir_ambe_server.desktop
 sed -i "10c Name[es_ES]=Abrir AMBE SERVER" /home/pi/Abrir_ambe_server.desktop
 sed -i "11c AMBE_SERVER=OFF" /home/pi/status.ini
@@ -34,4 +34,4 @@ sudo cp Abrir_ambe_server.desktop /home/pi/Desktop
 sleep 2
 sudo rm /home/pi/Abrir_ambe_server.desktop				
 						
-						
+
