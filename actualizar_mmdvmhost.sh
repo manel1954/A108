cb93cc04faced4365c25dc365a5b15716b72a560
ea3eiz <ea3eiz@ea3eiz.com>
2021-12-29 19:50:03 +0100

    1

--- actualizar_mmdvmhost.sh
+++ actualizar_mmdvmhost.sh
@@ -1,10 +1,10 @@
 ﻿#!/bin/bash
                     cd /home/pi
                     #sudo cp -R MMDVMHost /home/pi/.local/ 
-                    sudo rm -r /home/pi/.local/MMDVMHost
+                    #sudo rm -r /home/pi/.local/MMDVMHost
                     sudo apt-get install build-essential git-core libi2c-dev i2c-tools lm-sensors
-                    git clone https://github.com/g4klx/MMDVMHost
-                    cd MMDVMHost
+                    #git clone https://github.com/g4klx/MMDVMHost
+                    cd /home/pi/.local/MMDVMHost
                     git clone https://github.com/hallard/ArduiPi_OLED
                     cd ArduiPi_OLED
                     sudo make clean
@@ -23,19 +23,23 @@
                     make clean
                     make -f Makefile.Pi.OLED
 
+                    sudo cp Ŕ /home/pi/.local/MMDVMHost /home/pi
+
+                    sudo chmod 777 -R /home/pi/MMDVMHost
+
                     #Instala la secion [NextionDriver] en todos los .ini y todas sus memorias
                     cd /home/pi/NextionDriverInstaller
-                    sudo ./NextionDriver_ConvertConfig /home/pi/.local/MMDVMHost/MMDVM.ini
+                    sudo ./NextionDriver_ConvertConfig /home/pi/MMDVMHost/MMDVM.ini
                     sleep 2                  
                     
-                    sed -i "5c Duplex=0" /home/pi/.local/MMDVMHost/MMDVM.ini
-                    sed -i "52c # UARTSpeed=460800" /home/pi/.local/MMDVMHost/MMDVM.ini
-                    sed -i "229c Type=Direct" /home/pi/.local/MMDVMHost/MMDVM.ini
-                    sed -i "231c # LocalPort=62032" /home/pi/.local/MMDVMHost/MMDVM.ini
-                    sed -i "234c Password=PASSWORD" /home/pi/.local/MMDVMHost/MMDVM.ini
+                    sed -i "5c Duplex=0" /home/pi/MMDVMHost/MMDVM.ini
+                    sed -i "52c # UARTSpeed=460800" /home/pi/MMDVMHost/MMDVM.ini
+                    sed -i "229c Type=Direct" /home/pi/MMDVMHost/MMDVM.ini
+                    sed -i "231c # LocalPort=62032" /home/pi/MMDVMHost/MMDVM.ini
+                    sed -i "234c Password=PASSWORD" /home/pi/MMDVMHost/MMDVM.ini
 
                     # Crea los ejecutables para estas aplicaciones 
-                    cd /home/pi/.local/MMDVMHost
+                    cd /home/pi/MMDVMHost
                     cp MMDVMHost MMDVMBM
                     cp MMDVMHost MMDVMPLUS
                     cp MMDVMHost MMDVMDSTAR
@@ -93,15 +97,15 @@
                   
                     cp MMDVM.ini MMDVMDMRGateway.ini 
                     
-                    sed -i "52c # UARTSpeed=460800" /home/pi/.local/MMDVMHost/MMDVMDMRGateway.ini
-                    sed -i "229c Type=Gateway" /home/pi/.local/MMDVMHost/MMDVMDMRGateway.ini
-                    sed -i "231c LocalPort=62032" /home/pi/.local/MMDVMHost/MMDVMDMRGateway.ini
+                    sed -i "52c # UARTSpeed=460800" /home/pi/MMDVMHost/MMDVMDMRGateway.ini
+                    sed -i "229c Type=Gateway" /home/pi/MMDVMHost/MMDVMDMRGateway.ini
+                    sed -i "231c LocalPort=62032" /home/pi/MMDVMHost/MMDVMDMRGateway.ini
                     
-                    sed -i "52c # UARTSpeed=460800" /home/pi/.local/MMDVMHost/MMDVMDMR2YSF.ini
-                    sed -i "231c LocalPort=62032" /home/pi/.local/MMDVMHost/MMDVMDMR2YSF.ini
-                    sed -i "231c LocalPort=62032" /home/pi/.local/MMDVMHost/MMDVMDMR2NXDN.ini
-                    sed -i "231c LocalPort=62037" /home/pi/.local/MMDVMHost/MMDVMDMR2M17.ini
+                    sed -i "52c # UARTSpeed=460800" /home/pi/MMDVMHost/MMDVMDMR2YSF.ini
+                    sed -i "231c LocalPort=62032" /home/pi/MMDVMHost/MMDVMDMR2YSF.ini
+                    sed -i "231c LocalPort=62032" /home/pi/MMDVMHost/MMDVMDMR2NXDN.ini
+                    sed -i "231c LocalPort=62037" /home/pi/MMDVMHost/MMDVMDMR2M17.ini
 
-                    sudo chmod 777 -R /home/pi/.local/MMDVMHost
+                    sudo chmod 777 -R /home/pi/MMDVMHost
                 
                     exit;
