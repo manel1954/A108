7f668fcb9ce25c9b93b785002d35772caecada9c
manel1954 <ea3eiz@ea3eiz.com>
2022-04-05 17:06:04 +0200

    1

--- Open_ejecutar_solodstar.sh
+++ Open_ejecutar_solodstar.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-sudo ircddbgateway -gui &
-xterm -geometry 76x15+1296+560 -bg black -fg white -fa 'Serif' -fs 9x -T SOLO_DSTAR -e sudo sh Open_ejecutar_solodstar.sh
+
+# Ejecuta Solo D-STAR
+cd /home/pi/MMDVMHost
+#/home/pi/A108/./qt_info_solodstar & sudo ircddbgateway -gui & sudo ./MMDVMDSTAR MMDVMDSTAR.ini
+sudo ircddbgateway -gui & sudo ./MMDVMHost MMDVM.ini
+
 
 
 
