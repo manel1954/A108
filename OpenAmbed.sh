dd781f82c7f011977b968075341eb898ab7126e5
manel1954 <ea3eiz@ea3eiz.com>
2022-04-08 13:15:16 +0200

    1

--- OpenAmbed.sh
+++ OpenAmbed.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-cd /home/pi/release/build/
+cd /home/pi/release/build/xlx_ambed
+#!/bin/bash
+# start ambed server
 
-sudo xterm -geometry 61x10+249+97 -bg black -fg white -fa 'Serif' -fs 9x -T RADIO_TERMINAL -e sudo ambed 192.168.1.34 &
+sudo rmmod ftdi_sio
+
+sudo rmmod usbserial
+
+
+sudo xterm -geometry 61x16+249+97 -bg black -fg white -fa 'Serif' -fs 9x -T RADIO_TERMINAL -e sudo ambed 192.168.1.34 &
