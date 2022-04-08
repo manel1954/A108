89de2486076905bb7f1d70417c8a7e9c5591a741
manel1954 <ea3eiz@ea3eiz.com>
2022-04-08 12:06:36 +0200

    1

--- Open_ejecutar_solodstar.sh
+++ Open_ejecutar_solodstar.sh
@@ -3,9 +3,11 @@
 # Ejecuta Solo D-STAR
 cd /home/pi/MMDVMHost
 #/home/pi/A108/./qt_info_solodstar & sudo ircddbgateway -gui & sudo ./MMDVMDSTAR MMDVMDSTAR.ini 
-sudo ./MMDVMHost MMDVM.ini
-sleep 15
-sudo ircddbgateway -gui &
+sed -i '5c MMDVM=ON' /home/pi/status.ini
+sudo ./MMDVMHost MMDVM.ini &
+sleep 10
+sudo ircddbgateway -gui
+
 
 
 
