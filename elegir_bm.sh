4927340006e7d5ca376f8d1970c3ebc74b2eb5f0
manel1954 <ea3eiz@ea3eiz.com>
2022-07-27 19:51:04 +0200

    Update elegir_bm.sh

--- elegir_bm.sh
+++ elegir_bm.sh
@@ -1,19 +1,40 @@
 #!/bin/bash
 
-# Elegir  Abrir BM
+onoff=$(awk "NR==7" /home/pi/status.ini)
+if [ $onoff = MMDVMBM=ON ]
+then
+# Botón  Abrir BM ON
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_MMDVMBM.desktop /home/pi
 
-sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal --geometry=77x18 --title=BRANDMEISTER -e sudo sh ejecutar_bm.sh'" /home/pi/Abrir_MMDVMBM.desktop
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_BM_OFF.png" /home/pi/Abrir_MMDVMBM.desktop
-sed -i "10c Name[es_ES]=Abrir BM" /home/pi/Abrir_MMDVMBM.desktop
-sed -i "7c MMDVMBM=OFF" /home/pi/status.ini
+#sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal --geometry=77x18 --title=BRANDMEISTER -e sudo sh ejecutar_bm.sh'" /home/pi/Abrir_MMDVMBM.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_BM_ON.png" /home/pi/Abrir_MMDVMBM.desktop
+sed -i "10c Name[es_ES]=Cerrar BM" /home/pi/Abrir_MMDVMBM.desktop
 cd /home/pi
 
 sudo cp Abrir_MMDVMBM.desktop /home/pi/Desktop
 
 sudo rm /home/pi/Abrir_MMDVMBM.desktop
+
+else
+# Botón  Abrir BM OFF
+SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
+cd /home/pi/Desktop
+sudo cp Abrir_MMDVMBM.desktop /home/pi
+
+#sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal --geometry=77x18 --title=BRANDMEISTER -e sudo sh ejecutar_bm.sh'" /home/pi/Abrir_MMDVMBM.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_BM_OFF.png" /home/pi/Abrir_MMDVMBM.desktop
+sed -i "10c Name[es_ES]=Abrir BM" /home/pi/Abrir_MMDVMBM.desktop
+#sed -i "7c MMDVMBM=OFF" /home/pi/status.ini
+cd /home/pi
+
+sudo cp Abrir_MMDVMBM.desktop /home/pi/Desktop
+
+sudo rm /home/pi/Abrir_MMDVMBM.desktop
+fi
+
+
 
 # Elegir icono Editar BM
 cd /home/pi/Desktop
