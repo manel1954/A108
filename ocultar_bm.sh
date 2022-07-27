6accedcc8699bf4fa9211194cb26f6f4aa5a9052
manel1954 <ea3eiz@ea3eiz.com>
2022-07-27 13:40:38 +0200

    Update ocultar_bm.sh

--- ocultar_bm.sh
+++ ocultar_bm.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Ocultar icono Abrir BM
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_MMDVMBM.desktop /home/pi
@@ -10,3 +11,25 @@ sed -i "10c Name[es_ES]=." /home/pi/Abrir_MMDVMBM.desktop
 cd /home/pi
 sudo cp Abrir_MMDVMBM.desktop /home/pi/Desktop
 sudo rm /home/pi/Abrir_MMDVMBM.desktop
+
+# Ocultar icono Editar BM
+cd /home/pi/Desktop
+sudo cp Editar_MMDVMBM.desktop /home/pi
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;ls'" /home/pi/Editar_MMDVMBM.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_TRANSPARENTE.png" /home/pi/Editar_MMDVMBM.desktop
+sed -i "10c Name[es_ES]=." /home/pi/Editar_MMDVMBM.desktop
+
+cd /home/pi
+sudo cp Editar_MMDVMBM.desktop /home/pi/Desktop
+sudo rm /home/pi/Editar_MMDVMBM.desktop
+
+# Ocultar icono Editar info BM
+cd /home/pi/Desktop
+sudo cp RXF_BM.desktop /home/pi
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;ls'" /home/pi/RXF_BM.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_TRANSPARENTE.png" /home/pi/RXF_BM.desktop
+sed -i "10c Name[es_ES]=." /home/pi/RXF_BM.desktop
+
+cd /home/pi
+sudo cp RXF_BM.desktop /home/pi/Desktop
+sudo rm /home/pi/RXF_BM.desktop
