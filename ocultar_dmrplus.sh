8d9eae5f45b174821c5766118f10de00a644f677
manel1954 <ea3eiz@ea3eiz.com>
2022-07-27 21:04:28 +0200

    Update ocultar_dmrplus.sh

--- ocultar_dmrplus.sh
+++ ocultar_dmrplus.sh
@@ -5,7 +5,7 @@ SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_MMDVMPLUS.desktop /home/pi
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;ls'" /home/pi/Abrir_MMDVMPLUS.desktop
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_TRANSPARENTE.png" /home/pi/Abrir_MMDVMBM.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_TRANSPARENTE.png" /home/pi/Abrir_MMDVMPLUS.desktop
 sed -i "10c Name[es_ES]=." /home/pi/Abrir_MMDVMPLUS.desktop
 
 cd /home/pi
