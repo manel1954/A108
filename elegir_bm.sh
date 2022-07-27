1c12bbf297d59d7eb7adfd5856214c247c7f3ccb
manel1954 <ea3eiz@ea3eiz.com>
2022-07-27 17:29:11 +0200

    Update elegir_bm.sh

--- elegir_bm.sh
+++ elegir_bm.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Elegir  Abrir BM
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_MMDVMBM.desktop /home/pi
@@ -13,4 +14,26 @@ cd /home/pi
 sudo cp Abrir_MMDVMBM.desktop /home/pi/Desktop
 
 sudo rm /home/pi/Abrir_MMDVMBM.desktop
+
+# Elegir icono Editar BM
+cd /home/pi/Desktop
+sudo cp Editar_MMDVMBM.desktop /home/pi
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;./qt_editor_bm'" /home/pi/Editar_MMDVMBM.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_EDITAR_BM.png" /home/pi/Editar_MMDVMBM.desktop
+sed -i "11c Name[es_ES]=Editar BM" /home/pi/Editar_MMDVMBM.desktop
+
+cd /home/pi
+sudo cp Editar_MMDVMBM.desktop /home/pi/Desktop
+sudo rm /home/pi/Editar_MMDVMBM.desktop
+
+# Elegir icono Editar info BM
+cd /home/pi/Desktop
+sudo cp RXF_BM.desktop /home/pi
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;./qt_info_bm'" /home/pi/RXF_BM.desktop
+sed -i "6c Icon=/home/pi/$SCRIPTS_version/ICONO_INFO.png" /home/pi/RXF_BM.desktop
+sed -i "11c Name[es_ES]=." /home/pi/RXF_BM.desktop
+
+cd /home/pi
+sudo cp RXF_BM.desktop /home/pi/Desktop
+sudo rm /home/pi/RXF_BM.desktop
 
